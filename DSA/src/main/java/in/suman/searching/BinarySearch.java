e3a433391246b4713c2b6f13c66e9bd8587e70cb
Suman <suman.sde1922@gmail.com>
2025-07-08 12:54:19 +0530

    BinarySearch

--- DSA/src/main/java/in/suman/searching/BinarySearch.java
+++ DSA/src/main/java/in/suman/searching/BinarySearch.java
@@ -1,7 +1,35 @@
 package in.suman.searching;
 
 public class BinarySearch {
+
+    public static int binarySearch(int [] arr,int target){
+        int low =0;
+        int high = arr.length - 1;
+
+        while (low <= high){
+            int mid = low + (high - low) / 2; // To avoid overflow
+
+            if(arr[mid] == target){
+                return mid; // Target found
+            } else if(arr[mid] < target){
+                low = mid + 1; // Search in the right half
+            } else {
+                high = mid - 1; // Search in the left half
+            }
+        }
+        return -1; // Target not found
+    }
     public static void main(String[] args) {
+        int[] sortedArray = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+        int targetValue = 23;
+
+        int index = binarySearch(sortedArray, targetValue);
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
 
     }
 }
