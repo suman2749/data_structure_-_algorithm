58805ea94fb325fddbd90b9fde3dbb54c775aa36
Suman <suman.sde1922@gmail.com>
2025-07-08 11:24:21 +0530

    HeapSort

---
+++ DSA/src/main/java/in/suman/sorting/HeapSort.java
@@ -0,0 +1,65 @@
+package in.suman.sorting;
+
+public class HeapSort {
+
+    public void sort(int arr[]) {
+        int n = arr.length;
+
+        // Build max heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // One by one extract an element from heap
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    // To heapify a subtree rooted with node i which is an index in arr[].
+    // n is size of heap
+    void heapify(int arr[], int n, int i) {
+        int largest = i; // Initialize largest as root
+        int left = 2 * i + 1; // left child
+        int right = 2 * i + 2; // right child
+
+        // If left child is larger than root
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        // If right child is larger than largest so far
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    // Main method to test the HeapSort
+    public static void main(String args[]) {
+        int arr[] = {12, 11, 13, 5, 6, 7};
+        HeapSort ob = new HeapSort();
+        ob.sort(arr);
+
+        System.out.println("Sorted array is");
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
--- DSA/src/main/java/in/suman/sorting/HeapSorting.java
+++
@@ -1,7 +0,0 @@
-package in.suman.sorting;
-
-public class HeapSorting {
-    public static void main(String[] args) {
-
-    }
-}
